01346dc3d3f460dc482e2816e58c1fe4ecabc3d8
Byron Varberg <byronvarberg@gmail.com>
2024-10-10 12:58:25 -0500

    Configure deno as formatter for full workspace
    Until it becomes an issue with the mixed deno/npm directories, it's nice
    to have the formatting consistent without needing additional tooling.

--- apps/visualizer/src/App.tsx
+++ apps/visualizer/src/App.tsx
@@ -135,7 +135,7 @@ export default function App() {
                 />
               );
             default:
-              return null
+              return null;
           }
         })}
       </svg>
